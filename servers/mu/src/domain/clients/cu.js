0a941fd522e331744795df3e1aea30c802fa88dd
ppedziwiatr <ppedziwiatr@gmail.com>
2024-07-16 20:31:58 +0200

    fix: ooops i did it again

--- servers/mu/src/domain/clients/cu.js
+++ servers/mu/src/domain/clients/cu.js
@@ -9,6 +9,8 @@ const AO_TESTNET_PROCESS = [
   'fev8nSrdplynxom78XaQ65jSo7-88RxVVVPwHG8ffZk'
 ]
 
+const AO_TESTNET_CU_URL = 'https://cu.ao-testnet.xyz'
+
 function resultWith ({ fetch, histogram, CU_URL, logger }) {
   const resultFetch = withTimerMetricsFetch({
     fetch,
@@ -20,20 +22,24 @@ function resultWith ({ fetch, histogram, CU_URL, logger }) {
 
   return async (txId, processId, message) => {
     const cuUrl = AO_TESTNET_PROCESS.includes(processId)
-      ? 'https://cu.ao-testnet.xyz'
+      ? AO_TESTNET_CU_URL
       : CU_URL
 
     logger(`${cuUrl}/result/${txId}?process-id=${processId}&no-busy=1`)
 
-    const requestOptions = {
-      timeout: 0,
-      method: 'POST',
-      body: JSON.stringify(message),
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
+    const requestOptions = cuUrl === AO_TESTNET_CU_URL
+      ? {
+          timeout: 0
+        }
+      : {
+          timeout: 0,
+          method: 'POST',
+          body: JSON.stringify(message),
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          }
+        }
 
     return backoff(
       () =>
